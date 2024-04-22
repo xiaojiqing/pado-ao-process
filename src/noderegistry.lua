c30e5c4082b67e11e2e9a73516060fae16f30350
xiaojiqing <xiaojiqing@padolabs.org>
2024-04-22 04:17:57 -0700

    update node

--- src/noderegistry.lua
+++ src/noderegistry.lua
@@ -4,12 +4,60 @@ Handlers.add(
   "register",
   Handlers.utils.hasMatchingTag("Action", "Register"),
   function (msg)
-    Nodes[msg.Name] = Nodes[msg.Name] or {}
+    if msg.Name == nil then
+      Handlers.utils.reply("Name is required")(msg)
+      return
+    end
+
+    if msg.Publickey == nil then
+      Handlers.utils.reply("Publickey is required")(msg)
+      return
+    end
+
+    if msg.Desc == nil then
+      Handlers.utils.reply("Desc is required")(msg)
+      return
+    end
+
+    if Nodes[msg.Name] ~= nil then
+      Handlers.utils.reply("already registered")(msg)
+      return
+    end
+    Nodes[msg.Name] = {}
     Nodes[msg.Name].name = msg.Name
     Nodes[msg.Name].publickey = msg.Publickey
     Nodes[msg.Name].desc = msg.Desc
     Nodes[msg.Name].from = msg.From
     Handlers.utils.reply("registered")(msg)
+  end
+)
+
+Handlers.add(
+  "update",
+  Handlers.utils.hasMatchingTag("Action", "Update"),
+  function (msg)
+    if msg.Name == nil then
+      Handlers.utils.reply("Name is required")(msg)
+      return
+    end
+
+    if Nodes[msg.Name] == nil then
+      Handlers.utils.reply("record not exist")(msg)
+      return
+    end
+
+    if Nodes[msg.Name].from ~= msg.From then
+      Handlers.utils.reply("you are forbidden to update")(msg)
+      return
+    end
+
+    if msg.Publickey ~= nil then
+      Nodes[msg.Name].publickey = msg.Publickey
+    end
+
+    if msg.Desc ~= nil then
+      Nodes[msg.Name].desc = msg.Desc
+    end
   end
 )
 
