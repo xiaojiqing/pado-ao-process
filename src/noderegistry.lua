1f85e5148e88ae3ec4e8510c321993d93d513f4c
xiaojiqing <xiaojiqing@padolabs.org>
2024-04-22 18:45:08 -0700

    delete node

--- src/noderegistry.lua
+++ src/noderegistry.lua
@@ -1,5 +1,9 @@
 Nodes = Nodes or {}
 
+function get_node_key(msg)
+  return msg.From .. "-" .. msg.Name
+end
+
 Handlers.add(
   "register",
   Handlers.utils.hasMatchingTag("Action", "Register"),
@@ -19,16 +23,18 @@ Handlers.add(
       return
     end
 
-    if Nodes[msg.Name] ~= nil then
-      Handlers.utils.reply("already registered")(msg)
+    local node_key = get_node_key(msg)
+
+    if Nodes[node_key] ~= nil then
+      Handlers.utils.reply("already register " .. msg.Name)(msg)
       return
     end
-    Nodes[msg.Name] = {}
-    Nodes[msg.Name].name = msg.Name
-    Nodes[msg.Name].publickey = msg.Publickey
-    Nodes[msg.Name].desc = msg.Desc
-    Nodes[msg.Name].from = msg.From
-    Handlers.utils.reply("registered")(msg)
+    Nodes[node_key] = {}
+    Nodes[node_key].name = msg.Name
+    Nodes[node_key].publickey = msg.Publickey
+    Nodes[node_key].desc = msg.Desc
+    Nodes[node_key].from = msg.From
+    Handlers.utils.reply("register " .. msg.Name .. " by " .. msg.From)(msg)
   end
 )
 
@@ -41,24 +47,52 @@ Handlers.add(
       return
     end
 
-    if Nodes[msg.Name] == nil then
-      Handlers.utils.reply("record not exist")(msg)
+    local node_key = get_node_key(msg)
+
+    if Nodes[node_key] == nil then
+      Handlers.utils.reply("record " .. msg.Name .. " not exist")(msg)
       return
     end
 
-    if Nodes[msg.Name].from ~= msg.From then
+    if Nodes[node_key].from ~= msg.From then
       Handlers.utils.reply("you are forbidden to update")(msg)
       return
     end
 
     if msg.Publickey ~= nil then
-      Nodes[msg.Name].publickey = msg.Publickey
+      Nodes[node_key].publickey = msg.Publickey
     end
 
     if msg.Desc ~= nil then
-      Nodes[msg.Name].desc = msg.Desc
+      Nodes[node_key].desc = msg.Desc
     end
-    Handlers.utils.reply("updated")(msg)
+    Handlers.utils.reply("update " .. msg.Name .. " by " .. msg.From)(msg)
+  end
+)
+
+Handlers.add(
+  "Delete",
+  Handlers.utils.hasMatchingTag("Action", "Delete"),
+  function (msg)
+    if msg.Name == nil then
+      Handlers.utils.reply("Name is required")(msg)
+      return
+    end
+
+    local node_key = get_node_key(msg)
+
+    if Nodes[node_key] == nil then
+      Handlers.utils.reply("record " .. msg.Name .. " not exist")(msg)
+      return
+    end
+
+    if Nodes[node_key].from ~= msg.From then
+      Handlers.utils.reply("you are forbidden to delete")(msg)
+      return
+    end
+    Nodes[node_key] = nil
+
+    Handlers.utils.reply("delete " .. msg.Name .. " by " .. msg.From)(msg)
   end
 )
 
