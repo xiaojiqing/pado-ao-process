5c8be57bb6a5f131ac7d8611ddd7af9cb73745f6
fksyuan <fksyuan@163.com>
2024-04-24 10:25:46 +0800

    add tasks process

---
+++ src/tasks.lua
@@ -0,0 +1,48 @@
+AllTasks = AllTasks or {}
+PendingTasks = Tasks or {}
+
+Handlers.add(
+  "submit",
+  Handlers.utils.hasMatchingTag("Action", "Submit"),
+  function (msg)
+    PendingTasks[msg.Id] = {}
+    PendingTasks[msg.Id].id = msg.Id
+    PendingTasks[msg.Id].type = msg.Type
+    PendingTasks[msg.Id].inputData = msg.InputData
+    PendingTasks[msg.Id].computeLimit = msg.computeLimit
+    PendingTasks[msg.Id].memoryLimit = msg.memoryLimit
+    Handlers.utils.reply(msg.Id)(msg)
+  end
+)
+
+Handlers.add(
+  "getPendingTasks",
+  Handlers.utils.hasMatchingTag("Action", "GetPendingTasks"),
+  function (msg)
+    
+  end
+)
+
+Handlers.add(
+  "reportResult",
+  Handlers.utils.hasMatchingTag("Action", "ReportResult"),
+  function (msg)
+    
+  end
+)
+
+Handlers.add(
+  "getCompletedTasks",
+  Handlers.utils.hasMatchingTag("Action", "GetPendingTasks"),
+  function (msg)
+    
+  end
+)
+
+Handlers.add(
+  "getAllTasks",
+  Handlers.utils.hasMatchingTag("Action", "GetPendingTasks"),
+  function (msg)
+    
+  end
+)
